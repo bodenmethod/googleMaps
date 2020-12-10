8054a28aca0dc886c2d0dacdf1426f1b18dfce99
bodenmethod <60374163+bodenmethod@users.noreply.github.com>
2020-12-10 15:27:19 -0500

    Update index.php

--- googleMaps/index.php
+++ googleMaps/index.php
@@ -72,6 +72,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 </body>
+	
+	<!-- load the markerclustererplus library -->
+<script src="https://unpkg.com/@googlemaps/markerclustererplus/dist/index.min.js"></script>
 
 <!-- API KEY VERIFICATION SCRIPT FROM GOOGLE WITH CALLBACK PARAMETER -->
 <script
